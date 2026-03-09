e4c5be9d59678adda8879207db69e6d3819275f1
Sanni <117445321+fsannicolo@users.noreply.github.com>
2026-03-09 14:33:11 +0000

    feat: handler avanzati

--- hello-world/src/main/java/it/marconi/hello_world/controllers/ModelController.java
+++ hello-world/src/main/java/it/marconi/hello_world/controllers/ModelController.java
@@ -2,7 +2,12 @@ package it.marconi.hello_world.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,5 +17,27 @@ public class ModelController {
     @GetMapping("fixed")
     public ModelAndView handlerFixed() {
         return new ModelAndView("hello-world-model").addObject("maker", "A. Trentini");
+    }
+
+    @GetMapping("query") 
+    public ModelAndView handlerQuery(@RequestParam(defaultValue = "none") String param) {
+        return new ModelAndView("hello-world-model").addObject("maker", param);
+    }
+
+    @GetMapping("path/{username}")
+    public ModelAndView handlerPath(@PathVariable String username) {
+        return new ModelAndView("hello-world-model").addObject("maker", username);
+    }
+
+    // -------- per le seguenti richieste non è possibile usare un browser, serve un software di terze parti
+
+    @GetMapping(path = "header", headers = "myHeader")
+    public ModelAndView handlerHeader(@RequestHeader(defaultValue = "none") String myHeader) {
+        return new ModelAndView("hello-world-header").addObject("header", myHeader);
+    }
+
+    @PostMapping("body")
+    public ModelAndView handlerBody(@RequestBody String body) {
+        return new ModelAndView("hello-world-body").addObject("body", body);
     }
 }
